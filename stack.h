3a0cd948b13b8cf2f25c43df0b9ff2a6106f0590
Marvin <marvin.boo@student.mahidol.edu>
2025-11-18 03:55:40 +0000

    18th November

--- stack.h
+++ stack.h
@@ -2,6 +2,8 @@
 #ifndef stack_h
 #define stack_h
 #include "node.h"
+#include <iostream>
+
 class Stack {
 	NodePtr top;
 	int size;
@@ -15,11 +17,12 @@ public:
 
 void Stack::push(int x){
   NodePtr new_node=new NODE(x);
-  if(new_node){
-            // Left missing for exercises…
-   }
- 
-         // Left missing for exercises…
+    if (new_node) 
+    {
+        new_node->set_next(top);  
+        top = new_node;  
+        size++; 
+    }
     
 }
 
@@ -28,18 +31,31 @@ int Stack::pop(){
         int value;
         value=t->get_value();
     // Left missing part for exercises
+    
+        --size;
         delete t;
         return value;
-	//be careful of the empty stack!!!
+	
+        //be careful of the empty stack!!!
+
+    cout << "Empty Stack" << endl;
+    return 0;
     }
 
-Stack::Stack(){
-    //initialize stack
+Stack::Stack()
+{
+    top = NULL;
+    size = 0;
     
 }
-Stack::~Stack(){
-    //delete all remaning stack (i.e. pop all) 
-    
+Stack::~Stack()
+{
+    cout << "Clear the stack" << endl;
+    int n = size;
+    while (size>0)
+    {
+        pop();
+    }    
 }
 
 
